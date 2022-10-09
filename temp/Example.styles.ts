2530297c23e4b6b117d4d91e60dac9ef97e91d9c
Pedro <pedroklepa@idhl.co.uk>
2022-10-09 18:44:01 +0100

    Update Example file

--- temp/Example.styles.ts
+++ temp/Example.styles.ts
@@ -1,16 +1,9 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
-
-const Container = styled.section`
-	min-height: 100vh;
-`;
+const Container = styled.div``;
 
 const ExampleStyles = {
-	Wrapper,
 	Container,
 };
-
-// TODO: Remove this file once project is done
 
 export default ExampleStyles;
--- temp/Example.tsx
+++ temp/Example.tsx
@@ -1,17 +1,10 @@
-import PageHead from 'components/PageHead/PageHead';
 import * as React from 'react';
 import S from './Example.styles';
 
-// TODO: Remove this file once project is done
 export type ExampleProps = {};
 
 const Example: React.FC<ExampleProps> = () => {
-	return (
-		<S.Wrapper>
-			<PageHead title="Example title" description="Example description" />
-			<S.Container></S.Container>
-		</S.Wrapper>
-	);
+	return <S.Container></S.Container>;
 };
 
 export default Example;
